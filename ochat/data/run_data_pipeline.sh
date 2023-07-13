1d29872c158187a84596d2ef771e28d1311eefdd
Shuo Wang <315665262@qq.com>
2023-07-13 21:43:47 +0800

    Update run_data_pipeline.sh

--- ochat/data/run_data_pipeline.sh
+++ ochat/data/run_data_pipeline.sh
@@ -2,16 +2,18 @@
 
 INPUT_FOLDER=$1
 OUTPUT_FOLDER=$2
-MODEL_TYPES=("openchat" "openchat_8192" "opencoder")
-MODEL_PATHS=("openchat/openchat" "openchat/openchat_8192" "openchat/opencoderplus")
+# MODEL_TYPES=("openchat" "openchat_8192" "opencoder")
+# MODEL_PATHS=("openchat/openchat" "openchat/openchat_8192" "openchat/opencoderplus")
+MODEL_TYPES=("openchat")
+MODEL_PATHS=("openchat/openchat")
 
 # Clean and filter ShareGPT
 
-echo "Cleaning ShareGPT..."
-python -m ochat.data.clean_sharegpt --in-dir "$INPUT_FOLDER/ShareGPT" --out-file "$OUTPUT_FOLDER/sharegpt_clean.json"
+# echo "Cleaning ShareGPT..."
+# python -m ochat.data.clean_sharegpt --in-dir "$INPUT_FOLDER/ShareGPT" --out-file "$OUTPUT_FOLDER/sharegpt_clean.json"
 
-echo "Filtering ShareGPT..."
-python -m ochat.data.filter_sharegpt --in-file "$OUTPUT_FOLDER/sharegpt_clean.json" --out-file "$OUTPUT_FOLDER/sharegpt_gpt4.json"
+# echo "Filtering ShareGPT..."
+# python -m ochat.data.filter_sharegpt --in-file "$OUTPUT_FOLDER/sharegpt_clean.json" --out-file "$OUTPUT_FOLDER/sharegpt_gpt4.json"
 
 # Generate for model training
 
